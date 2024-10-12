bce05b45433c6d6c9b0949307b1f54788ced7c22
Prithiga <prithigacs2020@gmail.com>
2024-10-12 06:31:47 -0700

    Updates

--- src/main.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css'; // Optional, for styles
-
-ReactDOM.render(<App />, document.getElementById('root'));
